bf23e5a19ecc36f66698624b29f8658fc405ae8c
millerc3 <millerc3@vt.edu>
2018-03-31 15:15:43 -0400

    Update led.py

--- led.py
+++ led.py
@@ -14,54 +14,81 @@ GPIO.setup(pinRed, GPIO.OUT)
 GPIO.setup(pinGreen, GPIO.OUT)		
 GPIO.setup(pinBlue, GPIO.OUT)		
 
-clientConnected = True
+_host = 'host'
+_IP = '192.168.1.14'
+_user = 'user'
+_queue = 'led'
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+	host= 'localhost'))
+channel = connection.channel()
+channel.queue_declare(queue = _queue)
+
+
+print('[Checkpoint] Connected to vhost %s on RMQ server at %s as user %s', _host, _IP, _user')
+
+def callback(ch, method, properties, body):
+	print('[Checkpoint] Consuming from RMQ queue: %s', _queue)
+
+channel.basic_consume(callback,
+				      queue = _queue
+					  no_ack = False)
+
+channel.start_consuming()
+					  
+	# not too sure when to manipulate these
+clientConnected = False
 clientDisconnected = False
 orderSent = False
 orderStarted = False
 orderFinished = False
 
-colors = {
-	"red" : (1,0,0),
-	"green" : (0,1,0),
-	"bleu" : (0,0,1),
-	"purple" : (1,0,1),
-	"yellow" : (1,1,0)
-}
-
-# D = {"Key1": (1,2,3), "Key2": (4,5,6)}
-# D["Key2"][2]
-
-
 
 if clientConnected:
-	FlashLED(colors["blue"])
+	FlashLED("blue")
 
 if clientDisconnected:
-    FlashLED(colors["red"])
+    FlashLED("red")
 
 if orderSent:
-    FlashLED(colors["purple"])
+    FlashLED("purple")
     
 if orderStarted:
-    FlashLED(colors["yellow"])
+    FlashLED("yellow")
   
 if orderFinished:
-    FlashLED(colors["green"])
+    FlashLED("green")
     
     
     
-def FlashLED(red,green,blue):				#this doesnt work, cant get tuple to be the input for some reason
-    setRed = red
-    setGreen = green
-    setBlue = blue
-    
-    if setRed:
+def FlashLED(color):
+    if color == "blue":
+		print('[Checkpoint] Flashing LED to %s', color)
+        GPIO.output(pinRed, GPIO.LOW)
+		GPIO.output(pinGreen, GPIO.LOW)
+		GPIO.output(pinBlue, GPIO.HIGH)
+    if color == "red":
+		print('[Checkpoint] Flashing LED to %s', color)
         GPIO.output(pinRed, GPIO.HIGH)
-    if setGreen:
-        GPIO.output(pinGreen, GPIO.HIGH)
-    if setBlue:
-        GPIO.output(pinBlue, GPIO.HIGH)
-        
+		GPIO.output(pinGreen, GPIO.LOW)
+		GPIO.output(pinBlue, GPIO.LOW)
+	if color == "purple":
+		print('[Checkpoint] Flashing LED to %s', color)
+        GPIO.output(pinRed, GPIO.HIGH)
+		GPIO.output(pinGreen, GPIO.LOW)
+		GPIO.output(pinBlue, GPIO.HIGH)
+    if color == "yellow":
+		print('[Checkpoint] Flashing LED to %s', color)
+        GPIO.output(pinRed, GPIO.HIGH)
+		GPIO.output(pinGreen, GPIO.HIGH)
+		GPIO.output(pinBlue, GPIO.LOW) 
+	if color == "green":
+		print('[Checkpoint] Flashing LED to %s', color)
+        GPIO.output(pinRed, GPIO.LOW)
+		GPIO.output(pinGreen, GPIO.HIGH)
+		GPIO.output(pinBlue, GPIO.LOW)
+
+	 
     time.sleep(.25)
     
     GPIO.output(pinRed, GPIO.LOW)
