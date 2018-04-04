e4be2d07a1e0689e35c6c87d2550b199b48a9cb7
jkl0619 <pi@raspberrypi>
2018-04-04 00:39:45 +0000

    complete bluetooth processing

--- client.py
+++ client.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-
-# """A simple echo client"""
 import socket
 import optparse
 import pika
@@ -23,9 +21,9 @@ bluetooth = options.bluetooth
 #connect to bluetooth
 client_socket=BluetoothSocket(RFCOMM)
 client_socket.connect((bluetooth,3))
-print("[Checkpoint] Connecting to " + host, " on " + bluetooth)
+print(" [Checkpoint] Connecting to " + host, " on " + bluetooth)
 data = client_socket.recv(1024)
-
+data = data.decode('utf-8')
 print(" [Checkpoint] Received Menu: %r" % data)
 
 print("Enter items separated by space characters:")
@@ -38,11 +36,22 @@ orderID = client_socket.recv(1024)
 items = client_socket.recv(1024)
 totalPrice = client_socket.recv(1024)
 totalTime = client_socket.recv(1024)
+
+#Fix receiving items into appropriate cast type
+items = items.decode('utf-8')
+orderID = orderID.decode('utf-8')
+totalPrice = totalPrice.decode('utf-8')
+totalTime = totalTime.decode('utf-8')
+totalPrice = float(totalPrice)
+totalTime = float(totalTime)
 print(" [Checkpoint] Received receipt: ")
-print(orderID)
-print(items)
-print(totalPrice)
-print(totalTime)
+print("Order ID: " + orderID)
+print("Items: " + items)
+print("Total Price: " +str( totalPrice))
+print("Total Time: " + str(totalTime))
+
+client_socket.close()
+print(" [Checkpoint] Closed Bluetooth Connection")
 
 
 credentials = pika.PlainCredentials(param.rmq_params["username"], param.rmq_params["password"])
@@ -50,17 +59,16 @@ credentials = pika.PlainCredentials(param.rmq_params["username"], param.rmq_para
 #connect to the RabbitMQ
 connection = pika.BlockingConnection(pika.ConnectionParameters(host, credentials=credentials))
 channel = connection.channel()
-#should not declare exchanges or queues channel.queue_declare(queue='menu')
-print("[Checkpoint] Connected")
+
+print(" [Checkpoint] Connected to vhost " + param.rmq_params["vhost"], "on RMQ server at " + host, " as user " + param.rmq_params["username"])
 
 #define callback method
 def callback(ch, method, properties, body):
-    channel.basic_publish(exchange='', routing_key='OrderList', body=orderList)
-    print(" [Checkpoint] Sent Order: " + orderList)
+	print("Order Update: " + body)
 
-def callback_receipt(ch, method, properties, body):
-    print (" [Checkpoint] Received receipt: %r" % body)
+channel.queue_bind(exchange=param.rmq_params["exchange"],queue=orderID)
+channel.basic_consume(callback, queue=orderID, no_ack=True)
+print(" [Checkpoint] Consuming from RMQ Queue: " + orderID)
 
-#set our channel to receive messages from the queue
-#channel.basic_consume(callback, queue='hello', no_ack=True)
+channel.start_consuming()
 
