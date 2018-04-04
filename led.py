bfce3e7f26c350a92f9e1d58d9bba8550a518391
millerc3 <millerc3@vt.edu>
2018-04-04 02:59:13 +0000

    Update led.py

--- led.py
+++ led.py
@@ -1,96 +1,127 @@
 import RPi.GPIO as GPIO
 import time
 import pika
-import pybluez
-import libbluetooth-dev
-import rabbitmq-server
+import rmq_params
+import sys
 
-GPIO.setmode(GPIO.BOARD)		#Set to board referenced pins
-#set pins 11-13 as output pins
-pinRed = 11
-pinGreen = 13
-pinBlue = 15
+##function used to display a color to the LED
+def FlashLED(color, pinRed, pinGreen, pinBlue):
+    if color == "blue":
+        print('[Checkpoint] Flashing LED to ' + color)
+        GPIO.output(pinRed, GPIO.LOW)
+        GPIO.output(pinGreen, GPIO.LOW)
+        GPIO.output(pinBlue, GPIO.HIGH)
+    if color == "red":
+        print('[Checkpoint] Flashing LED to ' + color)
+        GPIO.output(pinRed, GPIO.HIGH)
+        GPIO.output(pinGreen, GPIO.LOW)
+        GPIO.output(pinBlue, GPIO.LOW)
+    if color == "purple":
+        print('[Checkpoint] Flashing LED to ' + color)
+        GPIO.output(pinRed, GPIO.HIGH)
+        GPIO.output(pinGreen, GPIO.LOW)
+        GPIO.output(pinBlue, GPIO.HIGH)
+    if color == "yellow":
+        print('[Checkpoint] Flashing LED to ' + color)
+        GPIO.output(pinRed, GPIO.HIGH)
+        GPIO.output(pinGreen, GPIO.HIGH)
+        GPIO.output(pinBlue, GPIO.LOW) 
+    if color == "green":
+        print('[Checkpoint] Flashing LED to ' + color)
+        GPIO.output(pinRed, GPIO.LOW)
+        GPIO.output(pinGreen, GPIO.HIGH)
+        GPIO.output(pinBlue, GPIO.LOW)
+
+    time.sleep(.25)
+
+    GPIO.output(pinRed, GPIO.LOW)
+    GPIO.output(pinGreen, GPIO.LOW)
+    GPIO.output(pinBlue, GPIO.LOW)
+
+
+GPIO.setwarnings(False)        #used to get rid of warnings from GPIO
+
+
+##argument parsing and error handling
+if sys.argv[1] == '-s':
+    _host = sys.argv[2]
+else:
+    print('Incorrect IP flag, format: ' + "'" + 'python led.py -s IP.ADDRESS -m PINMODE -r RED.PIN -g GREEN.PIN -b BLUE.PIN' + "'")
+
+if sys.argv[3] == '-m':
+    if sys.argv[4] == '10':
+        GPIO.setmode(GPIO.BOARD)
+    elif sys.argv[4] == '11':
+        GPIO.setmode(GPIO.BCM)
+    else:
+        print('Incorrect pin-mode flag, format: ' + "'" + 'python led.py -s IP.ADDRESS -m PINMODE -r RED.PIN -g GREEN.PIN -b BLUE.PIN' + "'")
+                
+if sys.argv[5] == '-r':
+    pinRed= int(sys.argv[6])
+else:
+    print('Incorrect red pin flag, format: ' + "'" + 'python led.py -s IP.ADDRESS -m PINMODE -r RED.PIN -g GREEN.PIN -b BLUE.PIN' + "'")
+
+if sys.argv[7] == '-g':
+    pinGreen = int(sys.argv[8])
+else:
+    print('Incorrect green pin flag, format: ' + "'" + 'python led.py -s IP.ADDRESS -m PINMODE -r RED.PIN -g GREEN.PIN -b BLUE.PIN' + "'")
+    
+if sys.argv[9] == '-b':
+    pinBlue = int(sys.argv[10])
+else:
+    print('Incorrect blue pin flag, format: ' + "'" + 'python led.py -s IP.ADDRESS -m PINMODE -r RED.PIN -g GREEN.PIN -b BLUE.PIN' + "'")
+        
+
+##set pins 11-13 as output pins
 GPIO.setup(pinRed, GPIO.OUT)		
 GPIO.setup(pinGreen, GPIO.OUT)		
 GPIO.setup(pinBlue, GPIO.OUT)		
 
-_host = 'host'
-_IP = '192.168.1.14'
-_user = 'user'
+##obtain information from the rmq_params file
+_user = rmq_params.rmq_params['username']
+_vhost = rmq_params.rmq_params['vhost']
+_pass = rmq_params.rmq_params['password']
+_exc = rmq_params.rmq_params['exchange']
 _queue = 'led'
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-	host= 'localhost'))
+##connect to the RMQ server
+credentials = pika.PlainCredentials(_user, _pass)
+connection = pika.BlockingConnection(pika.ConnectionParameters(host= _host, credentials = credentials, virtual_host = _vhost))
 channel = connection.channel()
-channel.queue_declare(queue = _queue)
+#channel.queue_bind(exchange = _exc, queue = rmq_params.rmq_params['led_queue'])
 
 
-print('[Checkpoint] Connected to vhost %s on RMQ server at %s as user %s', _host, _IP, _user')
+print('[Checkpoint] Connected to vhost ' + _vhost + ' on RMQ server at ' + _host + ' as user ' + _user)
       
-def callback(ch, method, properties, body):
-	print('[Checkpoint] Consuming from RMQ queue: %s', _queue)
 
-channel.basic_consume(callback,
-				      queue = _queue
-					  no_ack = False)
+print('[Checkpoint] Consuming from RMQ queue: ' + _queue)
 
-channel.start_consuming()
-					  
-	# not too sure when to manipulate these
-clientConnected = False
-clientDisconnected = False
-orderSent = False
-orderStarted = False
-orderFinished = False
+    ##constantly wait for LED updates
+while True:
+    def callback(ch, method, properties, body):        
+        if body.decode('utf-8') == 'CC':
+            FlashLED('blue', pinRed, pinGreen, pinBlue)
 
+        if body.decode('utf-8') == 'CD':
+            FlashLED("red", pinRed, pinGreen, pinBlue)
 
-if clientConnected:
-	FlashLED("blue")
+        if body.decode('utf-8') == 'OSe':
+            FlashLED("purple", pinRed, pinGreen, pinBlue)
 
-if clientDisconnected:
-    FlashLED("red")
+        if body.decode('utf-8') == 'OSt':
+            FlashLED("yellow", pinRed, pinGreen, pinBlue)
 
-if orderSent:
-    FlashLED("purple")
+        if body.decode('utf-8') == 'OF':
+            FlashLED("green", pinRed, pinGreen, pinBlue)
+            
+        ##get message
+    channel.basic_consume(callback,
+    		      queue = _queue,
+    		      no_ack = True)
     
-if orderStarted:
-    FlashLED("yellow")
-  
-if orderFinished:
-    FlashLED("green")
+    channel.start_consuming()
+
     
     
-    
-def FlashLED(color):
-    if color == "blue":
-		print('[Checkpoint] Flashing LED to %s', color)
-        GPIO.output(pinRed, GPIO.LOW)
-		GPIO.output(pinGreen, GPIO.LOW)
-		GPIO.output(pinBlue, GPIO.HIGH)
-    if color == "red":
-		print('[Checkpoint] Flashing LED to %s', color)
-        GPIO.output(pinRed, GPIO.HIGH)
-		GPIO.output(pinGreen, GPIO.LOW)
-		GPIO.output(pinBlue, GPIO.LOW)
-	if color == "purple":
-		print('[Checkpoint] Flashing LED to %s', color)
-        GPIO.output(pinRed, GPIO.HIGH)
-		GPIO.output(pinGreen, GPIO.LOW)
-		GPIO.output(pinBlue, GPIO.HIGH)
-    if color == "yellow":
-		print('[Checkpoint] Flashing LED to %s', color)
-        GPIO.output(pinRed, GPIO.HIGH)
-		GPIO.output(pinGreen, GPIO.HIGH)
-		GPIO.output(pinBlue, GPIO.LOW) 
-	if color == "green":
-		print('[Checkpoint] Flashing LED to %s', color)
-        GPIO.output(pinRed, GPIO.LOW)
-		GPIO.output(pinGreen, GPIO.HIGH)
-		GPIO.output(pinBlue, GPIO.LOW)
 
-	 
-    time.sleep(.25)
-    
-    GPIO.output(pinRed, GPIO.LOW)
-    GPIO.output(pinGreen, GPIO.LOW)
-    GPIO.output(pinBlue, GPIO.LOW)
+
